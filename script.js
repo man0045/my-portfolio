5f2fb8ffb4ce0155b4de59a3e58edc884ad2113d
Mannu Chaurasiya <mannuchaurasiya36@gmail.com>
2022-10-02 01:28:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("script running....")
+document.querySelector('.cross').style.display = 'none';
+document.querySelector('.hamburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('SidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('SidebarGo')){
+        document.querySelector('.ham').style.display='inline'
+        document.querySelector('.cross').style.display='none'
+    }
+    else{
+        document.querySelector('.ham').style.display='none'
+        setTimeout(()=>{
+        document.querySelector('.cross').style.display='inline'
+        }, 300);
+        
+    }
+}) ;
